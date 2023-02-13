b75bccbc01d1b79117f641ed2f3a7f67a511ac46
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-13 18:34:06 -0500

    retrieving all rows

--- introduction/app.js
+++ introduction/app.js
@@ -6,9 +6,16 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./test.db');
 
 
-db.all('SELECT * FROM test ORDER BY year', (error, rows) => {
+/*db.all('SELECT * FROM test ORDER BY year', (error, rows) => {
   if (error) {
     throw error;
   }
   printQueryResults(rows);
 });
+
+db.all('SELECT * FROM test WHERE year = 1866', (error, rows) => {
+  printQueryResults(rows);
+});*/
+
+
+
